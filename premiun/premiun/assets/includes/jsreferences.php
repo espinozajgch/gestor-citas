82053a10169056702cf369b5f5073c6ae345ed3d
espinozajgx <espinozajgx@gmail.com>
2018-11-29 22:08:40 -0300

    reiniciado

--- premiun/premiun/assets/includes/jsreferences.php
+++
@@ -1,6 +0,0 @@
-<script src="../../vendor/plugin/jquery/jquery.min.js"></script>
-<script src="../../vendor/plugin/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="../../vendor/plugin/jquery-ui/jquery-ui.js"></script>
-<script src="../../vendor/plugin/jquery-ui-custom/jquery-ui.min.js"></script>
-
-<!--script src="vendor/plugin/html5imageupload/html5imageupload.js"></script-->
