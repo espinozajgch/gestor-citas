fc2cafb0bbc178814ec70530e76144e1fcf3b795
espinozajgx <espinozajgx@gmail.com>
2019-01-05 18:59:43 -0300

    cambios

--- citas/assets/class/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start();
-session_destroy();
-unset($_SESSION["buscahogar_admin"]);
-unset($_SESSION["recuerdame_admin"]);
-//unset($_SESSION["id_rol"]);
-header("Location:../../index.php");
-?>
